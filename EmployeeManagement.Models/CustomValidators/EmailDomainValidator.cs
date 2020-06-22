88bdfe6da61f1bb970bc174e7d2fd101c4555e9a
srsaari <66336104+dbakepner@users.noreply.github.com>
2020-06-22 14:16:38 -0600

    Ok up to part 39 https://www.youtube.com/watch?v=tWXuOBNQh4o

---
+++ EmployeeManagement.Models/CustomValidators/EmailDomainValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EmployeeManagement.Models.CustomValidators
+{
+    public class EmailDomainValidator : ValidationAttribute
+    {
+        // incoming object is email address
+        public string AllowedDomain { get; set; }
+
+        // override IsValid().  
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] strings = value.ToString().Split('@');
+            if (strings.Length > 1 && strings[1].ToUpper() == AllowedDomain.ToUpper())
+            {
+                return null; //Return null indicates no errors occurred.
+            }
+            //return new ValidationResult($"Domain must be {AllowedDomain}", 
+            // put error message into the Employee.cs class instead.
+            return new ValidationResult(ErrorMessage, 
+                new[] { validationContext.MemberName });
+        }
+    }
+}
--- EmployeeManagement.Web/Models/EditEmployeeModel.cs
+++ EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -1,24 +1,32 @@
-﻿using EmployeeManagement.Models.CustomValidators;
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Models.CustomValidators;
+
 using System;
+
 using System.ComponentModel.DataAnnotations;
 
-namespace EmployeeManagement.Models
+
+namespace EmployeeManagement.Web.Models
 {
-    public class Employee
+    public class EditEmployeeModel
     {
+
         public int EmployeeId { get; set; }
-        
-        [Required(ErrorMessage ="First name must be provided and be at least 2 characters")]
+
+        [Required(ErrorMessage = "First name must be provided and be at least 2 characters")]
         [MinLength(2)]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last name must be provided and be at least 2 characters")]
         [MinLength(2)]
         public string LastName { get; set; }
-        
+
         [EmailAddress]
-        [EmailDomainValidator(AllowedDomain ="pragimtech.com", ErrorMessage ="Domain must be pragimtech.com")]
+        [EmailDomainValidator(AllowedDomain = "pragimtech.com", ErrorMessage = "Domain must be pragimtech.com")]
         public string Email { get; set; }
+
+        [CompareProperty("Email", ErrorMessage ="Email and Confirm Email must match.")]
+        public string ConfirmEmail { get; set; }
 
         [Required]
         public DateTime DateOfBirth { get; set; }
@@ -27,7 +35,8 @@ namespace EmployeeManagement.Models
         public int DepartmentId { get; set; }
         public string PhotoPath { get; set; }
 
+        // nav property - different table
         [ValidateComplexType]
-        public Department Department { get; set; } // nav property - different table
+        public Department Department { get; set; } = new Department(); // NEW or null ref exc
     }
 }
